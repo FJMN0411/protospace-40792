9d101e16312ace8c3c8863e1e2e44070b234d921
FJMN0411 <rstoyo.fujii.maina@gmail.com>
2024-05-01 18:26:53 +0900

    first commit

---
+++ app/controllers/prototypes_controller.rb
@@ -0,0 +1,61 @@
+class PrototypesController < ApplicationController
+  before_action :set_prototype, only: [:edit, :show]
+  before_action :move_to_index, except: [:index, :show]
+  
+
+  def index
+    @prototypes = Prototype.all
+  end
+
+  def new
+    @prototype = Prototype.new
+  end
+
+  def show
+    #@prototype = Prototype.find(params[:id])
+    @comment = Comment.new
+    @comments = @prototype.comments.includes(:user)
+  end
+
+  def edit
+    #@prototype = Prototype.find(params[:id])
+  end
+
+  def update
+    prototype = Prototype.find(params[:id])
+    prototype.update(prototype_params)
+    redirect_to root_path
+  end
+
+  def create
+    @prototype = Prototype.create(prototype_params)
+
+    if @prototype.save
+      redirect_to prototypes_path, notice: 'Prototype was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    prototype = Prototype.find(params[:id])
+    prototype.destroy
+    redirect_to root_path
+  end
+
+  private
+  def prototype_params
+    params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
+  end
+
+  def set_prototype
+    @prototype = Prototype.find(params[:id])
+  end
+
+  def move_to_index
+    unless current_user && current_user == Prototype.find(params[:id]).user
+      redirect_to root_path
+    end
+  end
+
+end
